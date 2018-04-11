2dcc440c44be8e805030d8125d3afc358e538130
priyakaushal <priya.kaushal@lexisnexis.com>
2018-04-11 16:27:48 +0530

    Signed-off-by: priyakaushal <priya.kaushal@lexisnexis.com>

--- MonolithicGenerator/runProcess.bat
+++ MonolithicGenerator/runProcess.bat
@@ -1,2 +1,2 @@
-java -jar MonolithicGenerator\\MonolithicGenerator.jar
+java -jar MonolithicGenerator\\MonolithicGenerator.jar C:\XSpec_Chanakya1\MonolithicGenerator
 pause read -p "Press [Enter] key to start backup..."
